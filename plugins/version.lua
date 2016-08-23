5d05ba92cae812ba4b4ae364b595fbfecbee070f
SwicastTeam <swicastbot@gmail.com>
2016-08-23 19:16:36 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return 'Swicast Bot v3 Supergroups\n\nAn Advanced Administration Boft Based On Telegram Bot\n\nSudo: 👑 ოг. ოαɴαɴ 👑\n\nMain Bot ID: @Swicast\n\nSwicast Support: 👤📝\n@Swicast_Support_Bot'
+  return 'Swicast Bot v3 Supergroups\n\nAn Advanced Administration Bot Based on Telegram Bot\n\nSudo: 👑 ოг. ოαɴαɴ 👑\n\nMain Bot ID: @Swicast\n\nSwicast Support: 👤📝\n@Swicast_Support_Bot'
 end
 
 return {
